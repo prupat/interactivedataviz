9645389b681da114d692c7be43f7e30f0fc79412
prupat <prudence.brou>
2022-02-21 15:43:04 -0500

    Tutorial Completed

--- 1_1_getting_started/main.js
+++ 1_1_getting_started/main.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const countUp = document.querySelector('.countUp button');
+const ResetZero = document.querySelector('.ResetZero button');
+let timeClick = document.querySelector('.Num h3');
+
+countUp. addEventListener('click', Compute);
+ResetZero. addEventListener('click', reset);
+
+function Compute(){
+    timeClick.innerHTML ++;
+}
+
+function reset(){
+    timeClick.innerHTML = 0;
+
+}
