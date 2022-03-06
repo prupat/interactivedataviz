c6374c1de5e78380204f4848347e2a8131ff9056
prupat <prudence.brou>
2022-03-06 18:27:48 -0500

    Linear scale bar chart

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,11 +1,46 @@
 /* CONSTANTS AND GLOBALS */
 // const width = ;
 // const height = ;
+var bar =[20,30, 50, 60, 70];
+var height = 400,
+    width = 600,
+    barWidth = 50,
+    barOffset = 5;
+
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(bar)])
+    .range([0,height]);
+
+
+  d3.select('#container').append('svg')
+     .attr('width', width)
+     .attr('height', height)
+     .style('background', '#c9d7d6')
+    .selectAll('rect'). data(bar)
+    .enter().append('rect')
+      .style('fill', '#00441b')
+      .attr('width', barWidth)
+      .attr('height', function(d){
+        return yScale(d);
+      })
+      .attr('x', function(d,i) {
+        return i*(barWidth + barOffset)
+      })
+      .attr('y', function(d) {
+        return height - yScale(d);
+      });
+
+
+
+
+
+
+
+
+
 
 /* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
-  .then(data => {
-    console.log("data", data)
+
 
     /* SCALES */
     /** This is where you should define your scales from data to pixel space */
@@ -13,5 +48,3 @@ d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
 
     /* HTML ELEMENTS */
     /** Select your container and append the visual elements to it */
-
-  })
