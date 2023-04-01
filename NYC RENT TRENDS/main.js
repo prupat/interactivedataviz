66592819733f6e3aa6286b72d4a6e834246004e7
prupat <prudence.brou>
2023-04-01 00:30:48 -0400

    HTML + JS changes 3

--- NYC RENT TRENDS/main.js
+++ NYC RENT TRENDS/main.js
@@ -4,13 +4,13 @@ const margin = {top: 50, right: 50, bottom: 50, left: 50},
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-const svg = d3.select("#chart")
+  svg = d3.select("#chart")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+          `translate(${margin.left},${margin.top})`);
 
 // Read the data
 d3.csv('../data/rent_data.csv', function(data) {
