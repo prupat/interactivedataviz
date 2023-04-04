c449005264d02dcd98d78f365dba424de01ddb70
prupat <prudence.brou>
2023-04-03 20:24:13 -0400

    new code

--- CODING TRIAL/main.js
+++ CODING TRIAL/main.js
@@ -3,100 +3,97 @@ const margin = {top: 20, right: 20, bottom: 30, left: 50},
 width = 960 - margin.left - margin.right,
 height = 500 - margin.top - margin.bottom;
 
-// declare and append the tooltip
-const tooltip = d3.select("#chart")
-.append("div")
-.attr("class", "tooltip")
-.style("opacity", 0);
+      // Load data from CSV file
+      d3.csv(
+        '../data/rent_data2.csv'
+,
+        (d) => {
+          return ({
+            city: d.City,
+            borough: d.Borough,
+            year: +d.Year,
+            month: d.Month.substring(2),
+            rentPrice: +d.RentPrice,
+          });
+        },
+        function (error, data) {
+          if (error) throw error;
 
-// Append the SVG object to the body of the page
-const svg = d3.select("#chart")
-.append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+          // Create options for dropdown lists
+          const yearOptions = Array.from(new Set(data.map((d) => d.year))).sort();
+          const cityOptions = Array.from(new Set(data.map((d) => d.city))).sort();
 
+          // Add options to dropdown lists
+          d3.select("#year")
+            .selectAll("option")
+            .data(yearOptions)
+            .enter()
+            .append("option")
+            .text((d) => d);
 
-// Parse the date / time
-const parseTime = d3.timeParse("%d-%b");
+          d3.select("#city")
+            .selectAll("option")
+            .data(cityOptions)
+            .enter()
+            .append("option")
+            .text((d) => d);
 
+          d3.select("#city2")
+            .selectAll("option")
+            .data(cityOptions)
+            .enter()
+            .append("option")
+            .text((d) => d);
 
-// Set the ranges
-const x = d3.scaleTime().range([0, width]);
-const y = d3.scaleLinear().range([height, 0]);
+          // Set default values for dropdown lists
+          let selectedYear = yearOptions[0];
+          let selectedCity = cityOptions[0];
+          let selectedCity2 = cityOptions[1];
 
+          // Update chart when dropdown list values change
+          d3.select("#year").on("change", updateChart);
+          d3.select("#city").on("change", updateChart);
+          d3.select("#city2").on("change", updateChart);
 
-// Define the line
-const valueline = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
+          // Set up the initial chart
+          updateChart();
 
+          function updateChart() {
+            // Get selected values from dropdown lists
+            selectedYear = d3.select("#year").property("value");
+            selectedCity = d3.select("#city").property("value");
+            selectedCity2 = d3.select("#city2").property("value");
 
-// Load the data
-d3.csv('../data/rent_data2.csv', function(error, data) {
-  if (error) throw error;
+            // Filter data based on selected values
+            const filteredData = data.filter(
+              (d) => d.year == selectedYear && (d.city == selectedCity || d.city == selectedCity2)
+            );
 
+            // Group data by month and city
+            const groupedData = d3.rollup(
+              filteredData,
+              (v) => d3.mean(v, (d) => d.rentPrice),
+              (d) => d.month,
+              (d) => d.city
+            );
 
-  // Format the data
-  data.forEach(function(d) {
-    d.date = parseTime(d.Year + "-" + d.Month);
-    d.close = +d.RentPrice;
-  });
+            // Set up scales
+            const xScale = d3
+              .scaleBand()
+              .domain(groupedData.keys())
+              .range([50, 750])
+              .paddingInner(0.1);
 
+            const yScale = d3
+              .scaleLinear()
+              .domain([1000, 11000])
+              .range([450, 50]);
 
-  // Filter the data by city and year
-  function filterData(city, year) {
-    return data.filter(function(d) {
-      return d.City === city && d.Year === year;
-    });
-  }
-
-  // Set the default city and year
-  const defaultCity = "All Downtown";
-  const defaultYear = "2010";
-
-
-  // Filter the data by default city and year
-  const filteredData = filterData(defaultCity, defaultYear);
-
-
-  // Scale the range of the data
-  x.domain(d3.extent(filteredData, function(d) { return d.date; }));
-  y.domain([0, d3.max(filteredData, function(d) { return d.close; })]);
-
-
-  // Add the X Axis
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-
-  // Add the Y Axis
-  svg.append("g")
-      .call(d3.axisLeft(y));
-
-  // Add the valueline path
-  svg.append("path")
-      .data([filteredData])
-      .attr("class", "line")
-      .attr("d", valueline);
-
-
-  // Add the dots
-  svg.selectAll("dot")
-      .data(filteredData)
-      .enter().append("circle")
-      .attr("r", 5)
-      .attr("cx", function(d) { return x(d.date); })
-      .attr("cy", function(d) { return y(d.close); })
-      .on("mouseover", function(d) {
-        tooltip.transition()
-               .duration(200)
-               .style("opacity", .9);
-        tooltip.html(d.Borough + "<br/>" + "$" + d.close + "<br/>" + d.Year)
-               .style("left", (d3.event.pageX + 5) + "px")
-               .style("top", (d3.event.pageY - 28) + "px");
-      })
-    })
+            // Set up line generator
+            const lineGenerator = d3
+              .line()
+              .x((d) => xScale(d[0]))
+              .y((d) => yScale(d[1]));
+            }
+          }
+      );
