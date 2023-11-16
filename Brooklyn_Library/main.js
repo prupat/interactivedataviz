fbe09a0c1ebaf379f8f6531f610d2e2a44337979
prupat <prudence.brou>
2023-11-16 00:54:45 -0500

    sidebar added

--- Brooklyn_Library/main.js
+++ Brooklyn_Library/main.js
@@ -19,6 +19,38 @@ fetch('../data/BK_Library.json')
             L.marker([parseFloat(position[0]), parseFloat(position[1])]).addTo(map)
                 .bindPopup(`<b>${name}</b><br>${address}<br>${description}<br>${hours}<br>${contact}`);
         });
+
+        // Function to populate the branch list in the sidebar
+function populateBranchList() {
+    const branchList = document.getElementById("branch-list");
+    
+    libraryData.forEach(branch => {
+        const listItem = document.createElement("li");
+        listItem.textContent = branch.data.title;
+        listItem.addEventListener("click", () => displayBranchOnMap(branch));
+        branchList.appendChild(listItem);
+    });
+}
+
+// Function to display branch information on the map and sidebar
+function displayBranchOnMap(branch) {
+    const branchName = document.getElementById("branch-name");
+    const branchAddress = document.getElementById("branch-address");
+    const branchHours = document.getElementById("branch-hours");
+    const branchWebsite = document.getElementById("branch-website");
+    
+    branchName.textContent = branch.data.title;
+    branchAddress.textContent = branch.data.address;
+    branchHours.textContent = branch.data.hours;
+    branchWebsite.href = branch.data.path;
+    
+    // Here, you can use Leaflet to display the branch on the map
+    // Use branch.data.position to set the marker position on the map
+    // Example: L.marker([latitude, longitude]).addTo(map);
+}
+
+// Call the function to populate the branch list when the page loads
+populateBranchList();
     })
     .catch(error => {
         console.error('Error loading JSON data:', error);
