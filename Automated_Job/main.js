1f63832c6989e5f1ae9b8dadd44167d0c54cb5f7
prupat <prudence.brou>
2024-01-24 23:15:16 -0500

    Ground Zero

---
+++ Automated_Job/main.js
@@ -0,0 +1,8 @@
+// set the dimensions and margins of the graph
+const width = window.innerWidth * .5,
+      height = window.innerHeight * .5,
+      margin = {top: 20, bottom: 60, left: 80, right: 60};
+
+
+
+
